ecd594f0c3e1e2247b7193663c996ba6ab2d4379
Frederik Hjorth <frederikhjorth@gmail.com>
2015-01-11 23:37:39 +0100

    figs

--- ms.R
+++ ms.R
@@ -5,8 +5,8 @@ erd<-read.csv(file="erd.csv")
 
 #country
 class(erd$v001e)
-erd$cntry<-as.factor(erd$v001e)
-levels(erd$cntry)<-c("Denmark","France","Germany","UK")
+erd$cntry<-as.factor(ifelse(erd$v001e %in% c(3,5,6,13,16,17),erd$v001e,NA))
+levels(erd$cntry)<-c("Denmark","France","Germany","Norway","Sweden","UK")
 
 #eff no of parties
 summary(erd$v309e)
@@ -30,26 +30,24 @@ erd$minority<-factor(ifelse(erd$v329e==1,1,0))
 #govt duration
 erd$reldur<-ifelse(erd$v603e<2,erd$v603e,NA)
 
-dkukfrde<-c(3,5,6,17)
-
-erd0<-erd
-erd<-erd0[erd0$v001e %in% dkukfrde,]
-
-
+#regulate line width
+erd$ltp<-as.character(ifelse(erd$cntry %in% c("Denmark","Norway","Sweden"),"d","s"))
 
 require(ggplot2)
-pdf(file="effnop.pdf",width=10,height=4)
-ggplot(erd,aes(x=fulldate,y=effnop,group=cntry)) +
-  geom_line(aes(color=cntry)) +
-  theme_bw()
+pdf(file="effnop2.pdf",width=10,height=4)
+ggplot(subset(erd,!is.na(cntry)),aes(x=fulldate,y=effnop,group=cntry)) +
+  geom_line(aes(color=cntry,linetype=ltp)) +
+  theme_bw() +
+  scale_colour_brewer(palette="Dark2")
 dev.off()
 
 
-pdf(file="minority.pdf",width=10,height=4)
-ggplot(erd,aes(x=fulldate,fill=minority)) +
+pdf(file="minority2.pdf",width=10,height=4)
+ggplot(subset(erd,!is.na(cntry)),aes(x=fulldate,fill=minority)) +
   geom_bar() +
   facet_grid(cntry~.) +
-  theme_bw()
+  theme_bw() +
+  scale_colour_brewer(palette="Dark2")
 dev.off()
 
 ggplot(erd,aes(x=fulldate,y=v407e,group=cntry)) +
@@ -90,17 +88,22 @@ ggplot(erd0,aes(x=erd0$v320e,y=erd0$v330e)) +
   theme_bw()
 dev.off()
 
-plot(erd0$v320e,erd0$v330e)
 
-summary(cabsizemodel<-lm(v330e~v320e,data=erd0))
-stargazer(cabsizemodel,style="apsr",align=T)
+names(erd)
+class(erd$v205e)
+erd$v205e[erd$cntry=="Denmark" & !is.na(erd$v205e)]
 
-table(erd$v600e)
+sdvot<-data.frame(cntry=c(erd$cntry[which(erd$cntry=="Denmark")],erd$cntry[which(erd$cntry=="Norway")],erd$cntry[which(erd$cntry=="Sweden")]),sdseats=c(erd$v205e[which(erd$cntry=="Denmark")],erd$v202e[which(erd$cntry=="Norway")],erd$v201e[which(erd$cntry=="Sweden")]),chambersize=c(erd$v519e[which(erd$cntry=="Denmark")],erd$v519e[which(erd$cntry=="Norway")],erd$v519e[which(erd$cntry=="Sweden")]),fulldate=c(erd$fulldate[which(erd$cntry=="Denmark")],erd$fulldate[which(erd$cntry=="Norway")],erd$fulldate[which(erd$cntry=="Sweden")]))
 
-ggplot(erd0,aes(x=fulldate,y=v600e,color=cntry)) +
-  geom_point() +
-  geom_smooth(method="lowess") +
+sdvot$cntry<-factor(sdvot$cntry,labels=c("Denmark","Norway","Sweden"))
+sdvot$seatshare<-sdvot$sdseats/sdvot$chambersize
+
+pdf(file="sdshare.pdf",width=10,height=4)
+ggplot(sdvot,aes(x=fulldate,seatshare)) +
+  geom_smooth(aes(colour=cntry)) +
+  geom_point(aes(colour=cntry)) +
+  scale_colour_brewer(palette="Dark2") +
+  xlab("") +
+  ylab("Social Democrats seat share") +
   theme_bw()
-
-
-
+dev.off()
